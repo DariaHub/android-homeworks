fb50767e84f097d8c99a59911b249795076e1be2
DariaHub <pomelova.dasha@mail.ru>
2021-12-28 22:53:05 +0300

    /n0

--- Daria Pomelova/L4/app/src/test/java/com/example/l4/ExampleUnitTest.kt
+++ Daria Pomelova/L4/app/src/test/java/com/example/l4/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.example.l4
 
+import org.junit.Assert
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
